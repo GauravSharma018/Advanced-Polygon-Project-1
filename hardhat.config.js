d296cdf0a8e1580021419c466d3150abac3d8f36
Gaurav Sharma <gaurav12345sharma1847@gmail.com>
2024-07-31 16:58:20 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,18 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 require("@nomiclabs/hardhat-ethers");
-module.exports = {
-  solidity: "0.8.18",
-};
 require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+  solidity: "0.8.20",
   networks: {
     amoy: {
       url: "https://rpc-amoy.polygon.technology",
       accounts: [process.env.PRIVATE_KEY],
     },
     sepolia: {
-      url: "https://1rpc.io/sepolia",
+      url: "https://ethereum-sepolia.blockpi.network/v1/rpc/public",
       accounts: [process.env.PRIVATE_KEY],
     },
   },

09686de101f8a4a64ef6304e55ea62cbcb44d467
Gaurav Sharma <gaurav12345sharma1847@gmail.com>
2024-07-31 16:59:32 +0530

    Update Mint.js

--- scripts/Mint.js
+++ scripts/Mint.js
@@ -1,23 +1,30 @@
+// This script batch mints ERC721A tokens.
 const { ethers } = require("hardhat");
 require("dotenv").config();
 
 async function main() {
   const privateKey = process.env.PRIVATE_KEY;
 
-  const networkAddress = "https://1rpc.io/sepolia";
+   // The  URL of the Sepolia test network
+  const networkAddress = "https://ethereum-sepolia.blockpi.network/v1/rpc/public";
 
+   // connect to the Ethereum network
   const provider = new ethers.providers.JsonRpcProvider(networkAddress);
 
+   // Create a signer (account) 
   const signer = new ethers.Wallet(privateKey, provider);
 
-  const contractAddress = "0x776824a42a841621761770e328A87C184c3b9595";
+  // The address of the deployed contract
+  const contractAddress = "0x01653DA3fb255de60adBA3D393450AB066383218";
 
+  // Get the contract factory for the contract and attach it to the signer
   const IndianNFT = await ethers.getContractFactory("police_cop", signer);
   const contract = await IndianNFT.attach(contractAddress);
 
+  // Call the mint function to mint 5 tokens
   await contract.mint(5);
 
-  console.log("Minted 5 tokens");
+  console.log("Successfully minted '5' tokens.");
 }
 
 main()

d0e403ee856067f18671bbf5f3f57434017634ea
Gaurav Sharma <gaurav12345sharma1847@gmail.com>
2024-07-31 17:01:12 +0530

    Update getBalance.js

--- scripts/getBalance.js
+++ scripts/getBalance.js
@@ -1,7 +1,7 @@
 const hre = require("hardhat");
 const tokenContractJSON = require("../artifacts/contracts/police_cop.sol/police_cop.json");
 
-const tokenAddress = "0xA65EF105e53F53634117EC39610559713A3d6019"; // Ethereum address of the deployed ERC721A contract
+const tokenAddress = "0x01653DA3fb255de60adBA3D393450AB066383218"; // Ethereum address of the deployed ERC721A contract
 const tokenABI = tokenContractJSON.abi;
 const walletAddress = "0xDB9e191ce1584B882c07b343348902Ac76c2F9bE"; // Ethereum public address for the wallet
 
